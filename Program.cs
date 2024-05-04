381f8af29a6bc745271125262157bba3d7293461
Average-AI-Enjoyer <156368107+Average-AI-Enjoyer@users.noreply.github.com>
2024-05-04 16:31:12 +0200

    Main Program upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.WebSockets;
+
+namespace Program;
+
+public class Program() {
+    static void Main(string[] args){
+        Console.WriteLine("Wilkommen Benutzer zu Schere, Stein, Papier!");
+        Console.WriteLine("Möchtest du einen Namen wählen? (j/n) ");
+        string? choice1 = Console.ReadLine();
+        string? name, choiceName;
+        if(choice1 == "j"){
+            do
+            {
+            Console.WriteLine("Dann wähle deinen Namen! ");
+            name = Console.ReadLine();
+            Console.WriteLine(name + ", ist das so richtig? (j/n) ");
+            choiceName = Console.ReadLine();
+            
+            }
+            while(choiceName != "j");
+            Console.WriteLine("Wilkommen " + name + "!");
+        }
+        else if(choice1 == "n"){
+            
+            name = "Benutzer";
+            Console.WriteLine("Dann wird dir der Name \""+name+"\" zugeteilt");
+        }    
+        else if (choice1 != "j" && choice1 != "n"){
+            Console.WriteLine("Ich habe das leider nicht verstanden...");
+            name = "Benutzer";
+            Console.WriteLine("Dann wird dir der Name \""+name+"\" zugeteilt");
+        }
+        else {
+            
+        }
+    }
+}
